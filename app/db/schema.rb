7502784db59b1bb275e252a4741bb07337e181be
ug <gr8.distance@gmail.com>
2024-02-29 22:58:42 +0900

    wip

--- app/db/schema.rb
+++ app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_29_132123) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_29_135747) do
   create_table "business_hours", force: :cascade do |t|
     t.integer "start_at", null: false
     t.integer "end_at", null: false
@@ -39,6 +39,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_29_132123) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["start_at", "end_at"], name: "index_reservations_on_start_at_and_end_at", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
